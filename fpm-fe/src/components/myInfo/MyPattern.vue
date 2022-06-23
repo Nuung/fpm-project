b6990b72b2faa7897f854e86444b293f6d32af55
Ijin Joo <48429380+jooijin@users.noreply.github.com>
2022-06-23 20:32:43 +0900

    프론트엔드 기본 구조 (#4)

---
+++ fpm-fe/src/components/myInfo/MyPattern.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>나의 소비패턴</h2>
+    <img :src="require(`@/assets/images/dummy.jpg`)" style="width:100vw"/>
+    <!--<span v-for="pattern in `${ getMyPattern }`" :key="pattern">{{ pattern }}</span>-->
+  </div>
+</template>
+
+<script>
+export default {
+    computed: {
+      /*
+        getMyPattern(){
+            // 내 패턴 가져옴
+            return ;
+        }*/
+    }
+}
+</script>
+
+<style>
+
+</style>
