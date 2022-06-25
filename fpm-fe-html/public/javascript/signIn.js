51ec53bf21dabe5a4408ed3d29a2ec8acd7d5085
HyeminBaek <ntn158@naver.com>
2022-06-25 17:06:54 +0900

    [update] login action

--- fpm-fe-html/public/javascript/signIn.js
+++ fpm-fe-html/public/javascript/signIn.js
@@ -1,25 +1,49 @@
 'use strict';
 
-const login = () => {
-  const form = document.loginForm;
-  const {id,pw} = form;
-  fetch("/api/user/login",{
+// event
+const loginEvent = () => {
+
+  const requestBody = {
+    'userId': document.getElementById("id").value,
+    'password': document.getElementById("pw").value
+  };
+
+  fetch("http://fpm.local/api/user/login",{
           method: 'POST',
           headers :{
               'Content-Type': 'application/json',
           },
-          body:{
-            'userId':id,
-            'password':pw
-          }
+          body: JSON.stringify(requestBody)
       }) 
       .then(response => {
+        // document.cookie = "jwt_token="+response['data']['token'];
+        if (response.ok && response.status == 201) {
+          alert("로그인 성공");  
           return location.href="mainPage.html";
+        }
+        else {
+          alert("로그인 실패, id pw 확인");  
+        }
       })
       .catch(err => {
-        console.log("로그인 실패! 메인 화면으로 돌아갑니다");
-        //return location.href="../index.html";
+        alert("로그인 실패! 메인 화면으로 돌아갑니다");
       });
 };
 
-login('bhm7266','');
+// dom ready init function
+const init = () => {
+  const loginBtn = document.getElementById("btn");
+  console.log(loginBtn);
+  loginBtn.addEventListener("click", (event) => {
+    loginEvent();
+  });
+};
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  
+  // Handler when the DOM is fully loaded
+  init();
+
+});
