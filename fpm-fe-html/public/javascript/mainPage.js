11547c741b5c2c0325e8069b77e5c67244003949
jooijin <48429380+jooijin@users.noreply.github.com>
2022-06-25 13:54:46 +0900

    메인페이지 데이터 일부 연동

--- fpm-fe-html/public/javascript/mainPage.js
+++ fpm-fe-html/public/javascript/mainPage.js
@@ -1,25 +1,70 @@
 $(document).ready(function () {
-  // json 형식으로 데이터 set
-  var params = {
-    name: $("#name").val(),
-    sex: $("#sex").val(),
-    age: $("#age").val(),
-    tellPh: $("#tellPh").val(),
-  };
-
-  // ajax 통신
   $.ajax({
-    type: "POST", // HTTP method type(GET, POST) 형식이다.
-    url: "/test/ajax", // 컨트롤러에서 대기중인 URL 주소이다.
-    data: params, // Json 형식의 데이터이다.
+    type: "GET",
+    url: "http://api.fpm.local/api/user/3wspvfvxsdn8ks3/hashtag",
+    headers: {
+      authorization:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjN3c3B2ZnZ4c2RuOGtzMyIsImlhdCI6MTY1NjEyMjUxNSwiZXhwIjoxNjg3NjU4NTE1LCJpc3MiOiJmcG0ifQ.tFeyTLzgLqqRWmdHGzfEMBCySuP48LkJ0aBhRtgyaTc",
+    },
     success: function (res) {
-      // 비동기통신의 성공일경우 success콜백으로 들어옵니다. 'res'는 응답받은 데이터이다.
-      // 응답코드 > 0000
-      alert(res.code);
+      //console.log(res);
+      for (let i = 1; i <= 5; i++) {
+        $("#hashtag" + i).text(res["user_hashtag"][i - 1]);
+      }
     },
     error: function (XMLHttpRequest, textStatus, errorThrown) {
-      // 비동기 통신이 실패할경우 error 콜백으로 들어옵니다.
       alert("통신 실패.");
+    },
+  });
+
+  $.ajax({
+    type: "GET",
+    url: "http://api.fpm.local/api/financial",
+    headers: {
+      authorization:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjN3c3B2ZnZ4c2RuOGtzMyIsImlhdCI6MTY1NjEyMjUxNSwiZXhwIjoxNjg3NjU4NTE1LCJpc3MiOiJmcG0ifQ.tFeyTLzgLqqRWmdHGzfEMBCySuP48LkJ0aBhRtgyaTc",
+    },
+    success: function (res) {
+      console.log(res);
+      let nickName = res["data"]["user"]["nickName"];
+      let totalAmt = res["data"]["userFinancialDetail"][0]["depositTotalAmt"];
+      let top3Spend = res["data"]["userFinancialDetail"][0]["spendCategory"]["after"];
+      $("#nickName").text(nickName);
+      $("#totalAmt").text(totalAmt.toLocaleString());
+      let idx = 1;
+      $.each(top3Spend, function(key, value){
+        //console.log(key, value);
+        if(idx > 3) return false;
+        $("#spend"+idx).text(key);
+        $("#spendAmt"+idx).text(value.toLocaleString());
+        idx++;
+      });
+    },
+    error: function (XMLHttpRequest, textStatus, errorThrown) {
+      alert("통신 실패.");
+    },
+  });
+
+  var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+  var yValues = [55, 49, 44, 24, 15];
+  var barColors = ["#b91d47", "#00aba9", "#2b5797", "#e8c3b9", "#1e7145"];
+
+  new Chart("myChart", {
+    type: "doughnut",
+    data: {
+      labels: xValues,
+      datasets: [
+        {
+          backgroundColor: barColors,
+          data: yValues,
+        },
+      ],
+    },
+    options: {
+      title: {
+        display: true,
+        text: "World Wide Wine Production 2018",
+      },
     },
   });
 });
