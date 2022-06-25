0ff7992089aaea43c7a92c273eec390c1c5acda2
HyeminBaek <ntn158@naver.com>
2022-06-26 00:28:44 +0900

    채팅방 구현

--- fpm-fe-html/public/javascript/chatting.js
+++ fpm-fe-html/public/javascript/chatting.js
@@ -1,6 +1,8 @@
 'use strict';
 
-// event
+let toUserId = '';
+
+// 메시지 불러오기
 const getUnreadMsg = () => {
     //const authorization = document.cookie;
     fetch("http://fpm.local/api/messages",{
@@ -9,29 +11,55 @@ const getUnreadMsg = () => {
               'Content-Type': 'application/json',
               'authorization':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImR3eGtxb2ExbWJ3eWw0ZCIsImlhdCI6MTY1NjE2Njc5NywiZXhwIjoxNjg3NzAyNzk3LCJpc3MiOiJmcG0ifQ.aixB_pLimGhEgoRTDNe-I7loPO8jPpDdkuDpqML9j8s'
           },
-      }) 
-      .then(response => {
-          return response.json();
-      })
-      .then((res) => {
+    }) 
+    .then(response => {
+        return response.json();
+    })
+    .then((res) => {
+        toUserId = res.allMessage[0]._id;
+        console.log(toUserId);
         console.log(res.allMessage[0]);
-      })
-      .catch(err => {
-        console.warn(err);
-      });
+    })
+    .catch(err => {
+    console.warn(err);
+    });
 
 };
 
+//상대방에게 채팅 보내기
 const sendMsg = () => {
-
+    const requestBody = {
+        "toUserId":toUserId,
+        "msg":document.getElementById('chatt').value
+    }
+    fetch("http://fpm.local/api/message",{
+        method: 'POST',
+        headers :{
+            'Content-Type': 'application/json',
+            'authorization':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImR3eGtxb2ExbWJ3eWw0ZCIsImlhdCI6MTY1NjE2Njc5NywiZXhwIjoxNjg3NzAyNzk3LCJpc3MiOiJmcG0ifQ.aixB_pLimGhEgoRTDNe-I7loPO8jPpDdkuDpqML9j8s'
+        },
+        body: JSON.stringify(requestBody)
+    }) 
+    .then(response => {
+        return getUnreadMsg();
+    })
+    .catch(err => {
+    console.warn(err);
+    });
+  
 }
 // dom ready init function
-const init = () => {
-    getUnreadMsg();
-    const sendBtn = document.getElementById("btn");
-    sendBtn.addEventListener("click", (event) => {
-        sendMsg();
-  });
+const init = async () => {
+    while(true) {
+        getUnreadMsg();
+
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        
+        const sendBtn = document.getElementById("sendBtn");
+        sendBtn.addEventListener("click", (event) => {
+            sendMsg();
+        });
+    }
 };
 
 
