a4ce17d9ff4e838de761aa085c94c7e164a427a9
Nuung <qlgks1@naver.com>
2022-06-25 20:53:48 +0900

    [create] update slider

---
+++ fpm-fe-html/public/javascript/challenge.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// dom ready init function
+const init = () => {
+    console.log("testst");
+    // $('.slick-div').slick({
+    //     dots: true,
+    //     infinite: true,
+    //     speed: 300,
+    //     slidesToShow: 1,
+    //     centerMode: true,
+    //     variableWidth: true
+    // });
+};
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Handler when the DOM is fully loaded
+    init();
+});
